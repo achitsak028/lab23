fc49f834a56cc9261ffce759be148a2a180fa2a2
achitsak028 <120167837+achitsak028@users.noreply.github.com>
2023-03-30 21:46:08 +0700

    complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,18 @@
-
+#include<iostream>
+using namespace std;
+int main(int argc,char* argv[]){
+int sum = 0;
+if(argc <= 1){
+cout << "Please input numbers to find average." << endl;
+return 0;
+}
+for(int i = 0;i < argc;i++){
+int ans = atoi(argv[i]);
+   sum += ans;
+}
+double result = (double)sum/(argc-1);
+    cout << "---------------------------------" << endl;
+    cout << "Average of " << argc-1 <<" numbers = " << result << endl;
+    cout << "---------------------------------";
+    return 0;
+} 
